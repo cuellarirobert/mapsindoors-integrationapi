8199cd7b0e7c9cea8b197f6986677962f27dd4ca
Robert Cuellari <43565981+cuellarirobert@users.noreply.github.com>
2022-11-02 21:31:58 +0100

    Update integration_api_instance.py
    updated to allow for instantiation without user/pass.

--- mapsindoors/integration_api_instance.py
+++ mapsindoors/integration_api_instance.py
@@ -1,36 +1,26 @@
 import requests
 import json
 from mapsindoors.url_classes import *
-from mapsindoors.auth_client import *
 from mapsindoors.geodata import *
 
 class ApiInstance:
-    def __init__(self, username, password, api_key):
-        self.username = username
-        self.password = password
+    def __init__(self, api_key):
         self.api_key = api_key
-        token = OAuthToken(username, password, api_key)
-        self.auth_object = token
-        self.url = token.url
-        self.access_token = token.access_token
+        self.url = Urls(api_key, response_format="json")
 
-    #returns the full datasest for all geodata objects in a solution
     def get_app_user_roles(self):
-    	response = requests.request("GET", url=self.url.app_user_roles_url(), headers={'Accept': 'application/json', 'Authorization': self.access_token})
+    	response = requests.request("GET", url=self.url.app_user_roles_url(), headers={'Accept': 'application/json'})
     	return response.json()
 
     #returns the full datasest for all geodata objects in a solution
     def get_raw_geodata(self):
-    	response = requests.request("GET", url=self.url.geodata_url(), headers={'Accept': 'application/json', 'Authorization': self.access_token})
+    	response = requests.request("GET", url=self.url.geodata_url(), headers={'Accept': 'application/json'})
     	return response.json()
 
     def get_location_types(self):
-        response = requests.request("GET", url=self.url.display_types_url(), headers={'Accept': 'application/json', 'Authorization': self.access_token})
+        response = requests.request("GET", url=self.url.display_types_url(), headers={'Accept': 'application/json'})
         return response.json()
 
     def get_categories(self):
-        response = requests.request("GET", url=self.url.categories_url(), headers={'Accept': 'application/json', 'Authorization': self.access_token})
+        response = requests.request("GET", url=self.url.categories_url(), headers={'Accept': 'application/json'})
         return response.json()
-
-    
-
